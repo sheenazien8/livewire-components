7c643a94e592a86699b1b6c74c56ac37fb1989b4
sheenazien8 <sheenazien08@gmail.com>
2021-02-26 21:24:37 +0700

    Init My Awesome Library

---
+++ src/LivewireComponentsServiceProiveder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sheenazien8\LivewireComponents;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class LivewireComponentsServiceProiveder
+ * @author sheenazien8
+ */
+class LivewireComponentsServiceProiveder extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
